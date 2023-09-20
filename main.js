9c5dfd90312301f6adb6e601edb8a2fc7dbe24f9
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-20 14:08:56 +0500

    js

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ btn.onclick = () => {};
 btn.onclick = () => {};
 const hellosalom = document.querySelector("selectors");
 const hellosalom = document.querySelector("selectors");
+const increment = document.querySelector(".btn");
+
+btn.onclick = () => {};
+
+const fgbd = document.querySelector(".btnfvb2");
+const fb = document.querySelector(".btnfb2");
+
+btn.onclick = () => {};
+btn.onclick = () => {};
+btn.onclick = () => {};
+btn.onclick = () => {};
+const hellosalom = document.querySelector("selectors");
+const hellosalom = document.querySelector("selectors");
