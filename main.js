5728c8d6626add040488165548e2254dae053aa4
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-20 10:13:13 +0500

    54c

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const increment = document.querySelector(".increment");
 const decrement = document.querySelector(".decrement");
+const fd = document.querySelector(".58");
+const wef = document.querySelector(".85");
