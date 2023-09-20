9341d521b9d1cf90b865d1a1a3c1a5bd2524b530
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-20 11:51:55 +0500

    codes

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ const increment = document.querySelector(".btn");
 
 btn.onclick = () => {};
 
-const button = document.querySelector(".btn2");
-const button = document.querySelector(".btn2");
-const button = document.querySelector(".btn2");
-const button = document.querySelector(".btn2");
 const fgbd = document.querySelector(".btnfvb2");
 const fb = document.querySelector(".btnfb2");
 
