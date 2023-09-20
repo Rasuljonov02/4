3851bef53a501829506c5df3809f0e2a571166bb
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-20 11:44:28 +0500

    add

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const increment = document.querySelector(".btn");
 btn.onclick = () => {};
 
 const button = document.querySelector(".btn2");
+const button = document.querySelector(".btn2");
+const button = document.querySelector(".btn2");
+const button = document.querySelector(".btn2");
