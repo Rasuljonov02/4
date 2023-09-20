54fde791c35e14fd58f9d72b6ab2f10f18414135
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-20 11:47:38 +0500

    salomm

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ const increment = document.querySelector(".btn");
 btn.onclick = () => {};
 
 const button = document.querySelector(".btn2");
+const fgbd = document.querySelector(".btnfvb2");const fb = document.querySelector(".btnfb2");
