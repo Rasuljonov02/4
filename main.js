e4587b4784f5cc724e34dab14e0f7ffaf96df75a
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-20 14:24:00 +0500

    js

--- main.js
+++ main.js
@@ -4,23 +4,14 @@ btn.onclick = () => {};
 
 const fgbd = document.querySelector(".btnfvb2");
 const fb = document.querySelector(".btnfb2");
-
 btn.onclick = () => {};
 btn.onclick = () => {};
 btn.onclick = () => {};
 btn.onclick = () => {};
 const hellosalom = document.querySelector("selectors");
 const hellosalom = document.querySelector("selectors");
-const increment = document.querySelector(".btn");
-
-btn.onclick = () => {};
-
-const fgbd = document.querySelector(".btnfvb2");
-const fb = document.querySelector(".btnfb2");
-
-btn.onclick = () => {};
-btn.onclick = () => {};
-btn.onclick = () => {};
-btn.onclick = () => {};
-const hellosalom = document.querySelector("selectors");
-const hellosalom = document.querySelector("selectors");
+export const OPERATIONS = ["+", "-", "*"];
+export const INTERVAL = 20;
+export const MAX_SECOND = 50;
+export const MIN_SECOND = 0;
+export const MAX_COUNT = 10;
