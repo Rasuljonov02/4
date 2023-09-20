4bb567ac6941e3ac75b039a2d07810b43983ec6a
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-20 14:11:21 +0500

    qwqqwds

--- main.js
+++ main.js
@@ -24,3 +24,8 @@ btn.onclick = () => {};
 btn.onclick = () => {};
 const hellosalom = document.querySelector("selectors");
 const hellosalom = document.querySelector("selectors");
+export const OPERATIONS = ["+", "-", "*"];
+export const INTERVAL = 20;
+export const MAX_SECOND = 50;
+export const MIN_SECOND = 0;
+export const MAX_COUNT = 10;
