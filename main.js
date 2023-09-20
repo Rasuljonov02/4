6109803445a333249873c7aa6d4050438d6dc1ff
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-20 14:03:31 +0500

    change js

--- main.js
+++ main.js
@@ -2,10 +2,4 @@ const increment = document.querySelector(".btn");
 
 btn.onclick = () => {};
 
-const fgbd = document.querySelector(".btnfvb2");
-const fb = document.querySelector(".btnfb2");
 
-btn.onclick = () => {};
-btn.onclick = () => {};
-btn.onclick = () => {};
-btn.onclick = () => {};
